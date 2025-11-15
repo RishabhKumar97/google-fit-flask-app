b46abf2d82bf3bace687108b87b4a1ded144e01f
RishabhKumar97 <rishak1997@gmail.com>
2025-11-15 13:35:10 +0530

    Dockerized the application

--- src/app.py
+++ src/app.py
@@ -1,25 +1,30 @@
-from flask import Flask, request, render_template
-import re
 import os
-import pandas as pd
-from datetime import date
+import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import date
 from typing import Dict, List
 
-from init import refresh_data_files_from_s3
+import pandas as pd
+from flask import Flask, render_template, request
+
+from init import s3_client
 
 app = Flask(__name__)
 
+
 class Config:
-    _metric_files_dir = os.path.join(os.getcwd(), 'data_files')
+    _metric_files_dir = os.path.join(os.getcwd(), "data_files")
 
     @staticmethod
     def gather_files() -> List[str]:
         """
         gather all metric files names in the data_files directory and return as a list
         """
-        return [os.path.join(Config._metric_files_dir, file_name) for file_name in os.listdir(Config._metric_files_dir)]
-    
+        return [
+            os.path.join(Config._metric_files_dir, file_name)
+            for file_name in os.listdir(Config._metric_files_dir)
+        ]
+
     @staticmethod
     def create_metric_file_mapping(files: List[str]) -> Dict[str, str]:
         """
@@ -30,6 +35,7 @@ class Config:
             mapping[re.search(r"^(\w+)_", file.split(os.sep)[-1]).group(1)] = file
         return mapping
 
+
 class MetricDataFrame:
     metric_files_dir = Config._metric_files_dir
 
@@ -39,10 +45,8 @@ class MetricDataFrame:
         create a pandas dataframe for a given metric from the mapping dict
         """
         try:
-            metric_df = (
-                pd.read_parquet(mapping[metric])
-            )
-            metric_df['metric'] = metric
+            metric_df = pd.read_parquet(mapping[metric])
+            metric_df["metric"] = metric
             return metric_df
         except Exception as e:
             print(f"Error loading metric data for {metric}: {e}")
@@ -52,19 +56,29 @@ class MetricDataFrame:
         """
         create a consolidated pandas dataframe for all metrics from the mapping dict
         """
-        all_metrics_df = pd.DataFrame(columns=['entity', 'metric', 'date', 'value'])
+        all_metrics_df = pd.DataFrame(columns=["entity", "metric", "date", "value"])
 
-        convert_to_date_col = lambda col: 'date' if col in ['date', 'month', 'week'] else col
+        convert_to_date_col = (
+            lambda col: "date" if col in ["date", "month", "week"] else col
+        )
 
         with ThreadPoolExecutor(max_workers=len(mapping)) as e:
             futures = list()
             for k in mapping.keys():
-                futures.append(e.submit(MetricDataFrame._get_metric_data, mapping, metric= k))
+                futures.append(
+                    e.submit(MetricDataFrame._get_metric_data, mapping, metric=k)
+                )
         for future in as_completed(futures):
             metric_df = future.result()
             metric_df.columns = list(map(convert_to_date_col, metric_df.columns))
-            metric_df['value'] = metric_df[[col for col in metric_df.columns if col not in ['entity', 'date', 'metric']]]
-            metric_df = metric_df[['entity', 'metric', 'date', 'value']]
+            metric_df["value"] = metric_df[
+                [
+                    col
+                    for col in metric_df.columns
+                    if col not in ["entity", "date", "metric"]
+                ]
+            ]
+            metric_df = metric_df[["entity", "metric", "date", "value"]]
             all_metrics_df = pd.concat([all_metrics_df, metric_df], ignore_index=True)
         return all_metrics_df
 
@@ -78,16 +92,22 @@ def get_json_from_data(
 ):
     """Returns a json string of the dataframe with the specified entities and date range."""
 
-    date_error_msg = {"status": "error", "message": "date must be provided as a string in ISO format (YYYY-MM-DD)."}
-    entity_error_msg = {"status": "error", "message": "one or more entities provided do not exist."}
+    date_error_msg = {
+        "status": "error",
+        "message": "date must be provided as a string in ISO format (YYYY-MM-DD).",
+    }
+    entity_error_msg = {
+        "status": "error",
+        "message": "one or more entities provided do not exist.",
+    }
     metric_error_msg = {"status": "error", "message": "metric provided do not exist."}
 
-    #entities and metric are required parameters
+    # entities and metric are required parameters
     if entities is None:
         return entity_error_msg
     if metric is None:
         return metric_error_msg
-    
+
     df_copy = df.copy()
 
     if isinstance(entities, str):
@@ -103,20 +123,20 @@ def get_json_from_data(
     for met in metric:
         if met not in all_metrics:
             return metric_error_msg
-             
+
     if isinstance(start_date, str):
         try:
             start_date = date.fromisoformat(start_date)
         except Exception:
             return date_error_msg
-        
+
     if isinstance(end_date, str):
         try:
             end_date = date.fromisoformat(end_date)
         except Exception:
             return date_error_msg
 
-    entity_filter = df_copy["entity"].isin(entities) 
+    entity_filter = df_copy["entity"].isin(entities)
     metric_filter = df_copy["metric"].isin(metric)
     date_col = (
         "date"
@@ -126,69 +146,74 @@ def get_json_from_data(
     start_date_filter = (df_copy[date_col] >= start_date) if start_date else True
     end_date_filter = (df_copy[date_col] <= end_date) if end_date else True
 
-    df_copy = df_copy[(entity_filter & start_date_filter & end_date_filter & metric_filter)]
-    
+    df_copy = df_copy[
+        (entity_filter & start_date_filter & end_date_filter & metric_filter)
+    ]
+
     # Format date column as plain date string
     df_copy[date_col] = df_copy[date_col].apply(lambda x: x.strftime("%Y-%m-%d"))
     return df_copy.to_json(orient="records", indent=4, date_format="iso")
-    
 
-@app.route('/metrics', methods=['GET'])
+
+@app.route("/metrics", methods=["GET"])
 def metrics():
     """API endpoint to get all available metrics."""
-    metrics = {
-        "metrics": list()
-    }
+    metrics = {"metrics": list()}
     for k in Config.create_metric_file_mapping(Config.gather_files()).keys():
-        metrics["metrics"].append({
-            "name": k
-            # "description": f"Description for {k} metric."
-        })
+        metrics["metrics"].append(
+            {
+                "name": k
+                # "description": f"Description for {k} metric."
+            }
+        )
     return metrics
 
 
-@app.route('/metrics/<string:metric_name>', methods=['GET'])
+@app.route("/metrics/<string:metric_name>", methods=["GET"])
 def get_metric_data(metric_name):
     """API endpoint to get metric data in JSON format based on query parameters."""
-    entities = request.args.get('entity', None)
-    start_date = request.args.get('start_date', None)
-    end_date = request.args.get('end_date', None)
+    entities = request.args.get("entity", None)
+    start_date = request.args.get("start_date", None)
+    end_date = request.args.get("end_date", None)
 
     mapping = Config.create_metric_file_mapping(Config.gather_files())
     all_metrics_df = MetricDataFrame.get_all_metrics_df(mapping)
     json_data = get_json_from_data(
         all_metrics_df,
-        entities=entities.split(',') if entities else None,
+        entities=entities.split(",") if entities else None,
         metric=metric_name,
         start_date=start_date,
-        end_date=end_date
+        end_date=end_date,
     )
     return json_data
 
-@app.route('/')
+
+@app.route("/")
 def home():
     """
     Available endpoints:
-    1. GET /metrics 
+    1. GET /metrics
         reponse: List of all available metrics.
     2. GET /metrics/<metric_name> with the below query params
     {
     "entity": "entity_name" (required, comma-separated for multiple),
     "start_date": "YYYY-MM-DD" (optional),
     "end_date": "YYYY-MM-DD" (optional)
-    } 
+    }
         response: Get metric data in JSON format.
-    """    
-    return render_template('index.html')
+    """
+    return render_template("index.html")
 
-@app.route('/refresh-metrics', methods=['POST'])
+
+@app.route("/refresh-metrics", methods=["POST"])
 def refresh():
     """Endpoint to refresh data files from S3."""
     import shutil
+
     shutil.rmtree(Config._metric_files_dir)
-    refresh_data_files_from_s3()
+    s3_client.refresh_data_files_from_s3()
     return {"status": "success", "message": "Data files refreshed from S3."}
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", debug=True)
- 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- src/init.py
+++ src/init.py
@@ -1,59 +1,81 @@
 import os
-import boto3
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
 
-s3_dir = 'activity-metric-plots'
-local_dir = 'data_files'
+s3_dir = "activity-metric-plots"
+local_dir = "data_files"
+
 
 class S3Client:
-    def __init__(self, bucket_name: str, aws_access_key_id: str, aws_secret_access_key: str) -> None:
-        self.s3 = boto3.client('s3', 
-                               aws_access_key_id=aws_access_key_id,
-                               aws_secret_access_key=aws_secret_access_key
+    def __init__(
+        self,
+        bucket_name: str = os.getenv("AWS_S3_BUCKET_NAME"),
+        aws_access_key_id: str = os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key: str = os.getenv("AWS_SECRET_ACCESS_KEY"),
+    ) -> None:
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
         )
         self.bucket_name = bucket_name
 
     def get_files(self, s3_dir: str) -> list[str]:
-        response = self.s3.list_objects_v2(Bucket=self.bucket_name)
+        response = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
         files = []
-        for object in response.get('Contents', []):
-            if object['Key'].startswith(s3_dir):
-                files.append(object['Key'])
+        for object in response.get("Contents", []):
+            if object["Key"].startswith(s3_dir):
+                files.append(object["Key"])
         return files
 
-    def download_file(self, key: str, local_path: str) -> None:
-        self.s3.download_file(self.bucket_name, key, local_path)
+    def download_file_from_bucket(self, key: str, local_path: str) -> None:
+        self.s3_client.download_file(self.bucket_name, key, local_path)
         print(f"Downloaded {key} to {local_path}")
 
-def refresh_data_files_from_s3() -> None:
-    os.makedirs(os.path.join(os.getcwd(), local_dir), exist_ok=True)
-
-    s3_client = S3Client(
-        bucket_name=os.getenv('AWS_S3_BUCKET_NAME'),
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-    )
-
-    files = s3_client.get_files(s3_dir= s3_dir)
-    file_download_futures = list()
-    with ThreadPoolExecutor(len(files)) as e:
-        for file_key in files:
-            local_path = os.path.join(os.getcwd(), local_dir, os.path.basename(file_key))
-            file_download_futures.append(
-                e.submit(
-                s3_client.download_file, 
-                file_key, 
-                local_path
+    def refresh_data_files_from_s3(self) -> None:
+        global s3_dir
+        os.makedirs(os.path.join(os.getcwd(), local_dir), exist_ok=True)
+        files = self.get_files(s3_dir=s3_dir)
+        file_download_futures = list()
+        with ThreadPoolExecutor(len(files)) as e:
+            for file_key in files:
+                local_path = os.path.join(
+                    os.getcwd(), local_dir, os.path.basename(file_key)
                 )
+                file_download_futures.append(
+                    e.submit(
+                        self.download_file_from_bucket,
+                        key=file_key,
+                        local_path=local_path,
+                    )
+                )
+        for future in as_completed(file_download_futures):
+            future.result()
+
+    def generate_presigned_file_upload_url(self, file_name: str, expiration=3000):
+        """
+        Generate a presigned URL S3 POST request to upload a file
+        """
+
+        # Generate a presigned S3 file upload URL
+
+        try:
+            response = self.s3_client.generate_presigned_post(
+                self.bucket_name,
+                file_name,
             )
-    for future in as_completed(file_download_futures):
-        future.result()
+            return response
+        except Exception as e:
+            print(f"Error generating presigned URL: {e}")
+            return None
 
-if __name__ == '__main__':
-    refresh_data_files_from_s3()
 
-    
-        
+s3_client = S3Client()
+if __name__ == "__main__":
+    # print(s3_client.__dict__)
+    s3_client.refresh_data_files_from_s3()
+    # print(s3_client.generate_presigned_file_upload_url(file_name="test_upload.txt"))
